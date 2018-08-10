de5cc5e30c94cb667363f153d3423221ae7902f5
Harry V. Torres <torresharry2@gmail.com>
2018-08-10 05:53:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your functions and code here
+
+
+//var Kittens = ['Milo', 'otis', 'Garfield'];
+
+function destructivelyAppendKitten(name){
+Kittens = ['Milo', 'otis', 'Garfield'];
+ var newArray = [];
+ var Kittens = newArray.concat(newArray);
+ Kittens.push('Ralph');
+    return Kittens;
+}
